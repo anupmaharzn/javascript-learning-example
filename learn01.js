d537d594ccf3d085d71acfec3c655731b6bd5387
anupmaharzn <hellen7203@gmail.com>
2021-11-17 16:34:29 +0545

    type and variable injavascript

--- learn01.js
+++ learn01.js
@@ -1,46 +1,46 @@
-// type and variable
-let x = 10;
-const y = true;
-const myObject = {
-    a: x,
-    b: y,
+// // type and variable
+// let x = 10;
+// const y = true;
+// const myObject = {
+//     a: x,
+//     b: y,
 
 
-}
-console.log(myObject);
+// }
+// console.log(myObject);
 
-x = 30;
-let anotherObject;
-anotherObject = {
-    newA: x,
-    b: y,
-    c: myObject
+// x = 30;
+// let anotherObject;
+// anotherObject = {
+//     newA: x,
+//     b: y,
+//     c: myObject
 
-};
+// };
 
-console.log(anotherObject);
-
-
-let myVariable;
-console.log(myVariable);
-
-myVariable = 10;
-
-console.log(myVariable);
-
-myVariable = true;
-console.log(myVariable);
-
-myVariable = {
-    x: true,
-    y: 10
-}
-
-console.log(myVariable);
+// console.log(anotherObject);
 
 
-//explicit conversion ko example ,ramro sanga handle garyena vani error auxa ni feri 
-//javascript is dynamic type so ,
-myVariable = null;
+// let myVariable;
+// console.log(myVariable);
 
-myVariable.x = false; //Uncaught TypeError: Cannot set properties of null (setting 'x')
+// myVariable = 10;
+
+// console.log(myVariable);
+
+// myVariable = true;
+// console.log(myVariable);
+
+// myVariable = {
+//     x: true,
+//     y: 10
+// }
+
+// console.log(myVariable);
+
+
+// //explicit conversion ko example ,ramro sanga handle garyena vani error auxa ni feri 
+// //javascript is dynamic type so ,
+// myVariable = null;
+
+// myVariable.x = false; //Uncaught TypeError: Cannot set properties of null (setting 'x')
